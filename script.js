cfe0de36c3a26d56f2cd667b4c4e081e3c2342cd
mandalsagnik <sagnikmandal001@gmail.com>
2024-08-02 12:06:28 +0530

    Web page codes

---
+++ script.js
@@ -0,0 +1,6 @@
+// Example: Alert when the 'JOIN US' button is clicked
+document.querySelector('.cn a').addEventListener('click', function(event) {
+    event.preventDefault();
+    alert('Join Us button clicked');
+});
+
